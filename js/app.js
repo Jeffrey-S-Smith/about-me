620643aa48b5ec2f22fa463083c0f7f9a08fe93e
Brooke Heck <b19heck@gmail.com>
2022-07-14 13:30:46 -0700

    question one is a function

--- js/app.js
+++ js/app.js
@@ -8,41 +8,43 @@ document.write (personName + ' Thank you for visting');
 /*console.log(personName + ' Thank you for visting');*/
 /*Question 1*/
 
-let livesInSeattle = prompt ('do you live in seattle ').toLowerCase();
+function askQuestionOne() {
+  let livesInSeattle = prompt ('do you live in seattle ').toLowerCase();
 
-if (livesInSeattle === 'yes' || livesInSeattle === 'y'){
-/*console.log('Yes I do live in seattle');*/
-  alert ('Yes I do live in seattle');
-
-} else if (livesInSeattle === 'no' || livesInSeattle === 'n') {
-
-  /*console.log ('sorry you are wrong I do live in seattle');*/
-  alert ('sorry you are wrong I do live in seattle');
-
-} else {
-
-  /*console.log ('You need to answer with a \'yes\' or a \'no\'');*/
-  alert ('You need to answer with a \'yes\' or a \'no\'');
+  if (livesInSeattle === 'yes' || livesInSeattle === 'y'){
+    /*console.log('Yes I do live in seattle');*/
+    alert ('Yes I do live in seattle');
+  } else if (livesInSeattle === 'no' || livesInSeattle === 'n') {
+    /*console.log ('sorry you are wrong I do live in seattle');*/
+    alert ('sorry you are wrong I do live in seattle');
+  } else {
+    /*console.log ('You need to answer with a \'yes\' or a \'no\'');*/
+    alert ('You need to answer with a \'yes\' or a \'no\'');
+  }
 }
+
+askQuestionOne();
 
 
 /*Question 2 took example from https://sebhastian.com/javascript-confirmation-yes-no/#:~:text=You%20can%20create%20a%20JavaScript,can%20specify%20as%20its%20argument.*/
 
-function showConfirmBox() {
-  document.getElementById('overlay').hidden = false;
-}
-function closeConfirmBox() {
-  document.getElementById('overlay').hidden = true;
-}
+// function showConfirmBox() {
+//   document.getElementById('overlay').hidden = false;
+// }
+// function closeConfirmBox() {
+//   document.getElementById('overlay').hidden = true;
+// }
 
-function isConfirm(answer) {
-  if (answer) {
-    alert('yes is the wrong answer');
-  } else {
-    alert('no is the correct answer From Charlottesville VA.');
-  }
-  closeConfirmBox();
-}
+// function isConfirm(answer) {
+//   if (answer) {
+//     alert('yes is the wrong answer');
+//   } else {
+//     alert('no is the correct answer From Charlottesville VA.');
+//   }
+//   closeConfirmBox();
+// }
+
+
 
 /*Question 3 */
 function showConfirmBox() {
