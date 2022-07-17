3a9c72b0c519f16555e4daa95037cdd37fa819a5
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-07-17 04:04:07 -0700

    Work experience and education into unorderlist added show user how many question were correct was added added css styling

--- js/app.js
+++ js/app.js
@@ -4,26 +4,25 @@ let personName = prompt('What is your name?', 'Type your name here');
 
 // Show personalized message
 alert('Hi, ' + personName + ' It is very nice to meet you. Welcome to my site.');
-document.write (personName + ' Thank you for visting');
-/*console.log(personName + ' Thank you for visting');*/
+document.write(personName + ' Thank you for visting');
+
 
 /* Show how many questions are right. 7 question*/
-let numCorrect = 0; 
+let numCorrect = 0;
 
 /*Question 1*/
 function askQuestionOne() {
   let livesInSeattle = prompt ('do you live in seattle ').toLowerCase();
 
   if (livesInSeattle === 'yes' || livesInSeattle === 'y'){
-    /*console.log('Yes I do live in seattle');*/
     alert ('Yes I do live in seattle');
     /* Show how many questions are right 7 question*/
-    numCorrect++; 
+    numCorrect++;
   } else if (livesInSeattle === 'no' || livesInSeattle === 'n') {
-    /*console.log ('sorry you are wrong I do live in seattle');*/
+
     alert ('sorry you are wrong I do live in seattle');
   } else {
-    /*console.log ('You need to answer with a \'yes\' or a \'no\'');*/
+
     alert ('You need to answer with a \'yes\' or a \'no\'');
   }
 }
@@ -35,15 +34,15 @@ function askQuestionTwo() {
   let fromWashington = prompt ('Am I from Washington ').toLowerCase();
 
   if (fromWashington === 'yes' || fromWashington === 'y'){
-    /*console.log('No I'm from Virginia');*/
+
     alert ('No, sorry I\'m from Virgina');
   } else if (fromWashington === 'no' || fromWashington === 'n') {
-    /*console.log ('That's correct, I'm from virginia');*/
-    alert ('Yes, that\'s correct, I\' from Virginia.');
+
+    alert ('Yes, that\'s correct, I\'m from Virginia.');
     /* Show how many questions are right. 7 question*/
     numCorrect++;
   } else {
-    /*console.log ('You need to answer with a \'yes\' or a \'no\'');*/
+
     alert ('You need to answer with a \'yes\' or a \'no\'');
   }
 }
@@ -55,15 +54,15 @@ function askQuestionThree() {
   let fearOfHeights = prompt ('Is fear of heights my number one fear?').toLowerCase();
 
   if (fearOfHeights === 'yes' || fearOfHeights === 'y'){
-    /*console.log('No, I"m not afraid of heights');*/
+
     alert ('That\'s incorrect, I\'m not afraid of heights');
   } else if (fearOfHeights === 'no' || fearOfHeights === 'n') {
-    /*console.log ('Yes that's correct, I'm not afraid of heights');*/
-    alert ('Yes, that\'s correct, I\' not afraid of heights.');
+
+    alert ('Yes, that\'s correct, I\'m not afraid of heights.');
     /* Show how many questions are right. 7 question*/
     numCorrect++;
   } else {
-    /*console.log ('You need to answer with a \'yes\' or a \'no\'');*/
+
     alert ('You need to answer with a \'yes\' or a \'no\'');
   }
 }
@@ -74,15 +73,15 @@ askQuestionThree();
 function askQuestionFour() {
   let woodworking = prompt ('Do I enjoy woodworking?').toLowerCase();
   if (woodworking === 'yes' || woodworking === 'y'){
-    /*console.log('Yes I do enjoy woodworking');*/
+
     alert ('Yes I do enjoy woodworking');
     /* Show how many questions are right. 7 question*/
     numCorrect++;
   } else if (woodworking === 'no' || woodworking === 'n') {
-  /*console.log ('sorry you are wrong');*/
+
     alert ('sorry you are wrong');
   } else {
-  /* console.log ('You need to answer with a \'yes\' or a \'no\'');*/
+
     alert ('You need to answer with a \'yes\' or a \'no\'');
   }
 }
@@ -95,16 +94,16 @@ function askQuestionFive() {
   let technology = prompt ('Do I like Technology?').toLowerCase();
   if (technology === 'yes' || technology === 'y'){
 
-    /*console.log('Yes I like technology');*/
+
     alert ('Yes I do enjoy technology');
     /* Show how many questions are right. 7 question*/
     numCorrect++;
 
   } else if (technology === 'no' || technology === 'n') {
-  /*console.log ('sorry you are wrong');*/
+
     alert ('sorry you are wrong');
-  } else { 
-    /* console.log ('You need to answer with a \'yes\' or a \'no\'');*/
+  } else {
+
     alert ('You need to answer with a \'yes\' or a \'no\'');
   }
 }
@@ -122,7 +121,7 @@ function askQuestionSix() {
         ' You have ' + turns + ' guesses left.');
     if (!guess) break;
     guess = Number(guess);
-    console.log(typeof guess);
+
     if (guess === x) {
       document.write('<p>YOU WIN!</p>');
       /* Show how many questions are right. 7 question*/
@@ -149,10 +148,10 @@ function askQuestionSeven() {
   guessInput = prompt('What American Football Teams have I visited?');
   /* userResponse === seahawks then console.log (correct answer YOU WIN!)*/
   while (guessRemaining) {
-    console.log('You have ${guessRemaining} guesses left');
+
     for (let i = 0; i < myFavAFootball.length; i++) {
       if (guessInput.toLowerCase() === myFavAFootball[i].toLowerCase()) {
-        console.log('your correct');
+
         /* Show how many questions are right. 7 question*/
         numCorrect++;
         guessRemaining = 0;
